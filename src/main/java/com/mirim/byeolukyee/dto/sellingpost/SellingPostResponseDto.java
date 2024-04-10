5651b1fb233ded9d746815ea207dff9815b44179
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-10 10:46:21 +0900

    feat: 상품 이미지 리스트 추가

--- src/main/java/com/mirim/byeolukyee/dto/sellingpost/SellingPostResponseDto.java
+++ src/main/java/com/mirim/byeolukyee/dto/sellingpost/SellingPostResponseDto.java
@@ -2,24 +2,29 @@ package com.mirim.byeolukyee.dto.sellingpost;
 
 import com.mirim.byeolukyee.constant.SellingPostStatus;
 import com.mirim.byeolukyee.dto.post.PostResponseDto;
+import com.mirim.byeolukyee.dto.postimage.PostImageResponseDto;
 import com.mirim.byeolukyee.dto.user.UserResponseDto;
 import com.mirim.byeolukyee.domain.SellingPost;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class SellingPostResponseDto extends PostResponseDto {
 
     private final SellingPostStatus status;
     private final String krStatus;
+    private final List<PostImageResponseDto> images;
 
     @Builder
-    public SellingPostResponseDto(Boolean isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt, Long id, UserResponseDto user, String title, String description, Integer price, String location, SellingPostStatus status) {
+    public SellingPostResponseDto(Boolean isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt, Long id, UserResponseDto user, String title, String description, Integer price, String location, SellingPostStatus status, List<PostImageResponseDto> images) {
         super(isDeleted, createdAt, updatedAt, id, user, title, description, price, location);
         this.status = status;
         this.krStatus = status.getKrName();
+        this.images = images;
     }
 
     public static SellingPostResponseDto from(SellingPost sellingPost) {
@@ -34,6 +39,7 @@ public class SellingPostResponseDto extends PostResponseDto {
                 .price(sellingPost.getPrice())
                 .location(sellingPost.getLocation())
                 .status(sellingPost.getStatus())
+                .images(sellingPost.getPostImageList().stream().map(PostImageResponseDto::from).collect(Collectors.toList()))
                 .build();
     }
 

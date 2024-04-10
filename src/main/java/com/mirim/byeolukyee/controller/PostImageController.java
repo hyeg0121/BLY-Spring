069bd9a46359a70c8d0aa0b9471e853ae9fa9fb4
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-10 09:29:01 +0900

    feat: PostImageController 이미지 업로드 로직 추가

---
+++ src/main/java/com/mirim/byeolukyee/controller/PostImageController.java
@@ -0,0 +1,31 @@
+package com.mirim.byeolukyee.controller;
+
+import com.mirim.byeolukyee.dto.postimage.PostImageResponseDto;
+import com.mirim.byeolukyee.service.PostImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.IOException;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/post-images")
+@RequiredArgsConstructor
+public class PostImageController {
+    private final PostImageService postImageService;
+
+    @PostMapping(value = "/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<List<PostImageResponseDto>> uploadPostImage(
+            @PathVariable Long id,
+            @RequestParam("file") List<MultipartFile> files
+    ) throws IOException {
+
+
+        return ResponseEntity.ok().body(postImageService.uploadPostImage(id, files));
+    }
+}

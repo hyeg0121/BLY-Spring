6c397394bdc922cd975d34e13f7f81679e2ca0b3
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-03 18:11:58 +0900

    feat: 이미지 업로드 방식 변경 (s3버킷 사용)

--- src/main/java/com/mirim/byeolukyee/controller/PostImageController.java
+++ src/main/java/com/mirim/byeolukyee/controller/PostImageController.java
@@ -3,8 +3,6 @@ package com.mirim.byeolukyee.controller;
 import com.mirim.byeolukyee.dto.postimage.PostImageResponse;
 import com.mirim.byeolukyee.service.PostImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 
@@ -24,28 +21,10 @@ public class PostImageController {
 
     @PostMapping(value = "/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<List<PostImageResponse>> uploadPostImage(
-            @PathVariable Long id,
+            @PathVariable("id") Long id,
             @RequestParam("file") List<MultipartFile> files
     ) throws IOException {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(postImageService.uploadPostImage(id, files));
     }
-
-    @GetMapping("/{uploadedFileName}")
-    @ResponseBody
-    public ResponseEntity<Resource> getImage(@PathVariable("id") String uploadedFileName) throws IOException {
-        Resource imageResource = postImageService.getImageResource(uploadedFileName);
-
-        // 이미지 파일의 MIME 타입 추정
-        String contentTypeStr = Files.probeContentType(imageResource.getFile().toPath());
-        MediaType contentType = MediaType.parseMediaType(contentTypeStr);
-
-        // ResponseEntity에 Resource 객체와 헤더 정보 설정하여 반환
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(contentType);
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(imageResource);
-    }
-
 }

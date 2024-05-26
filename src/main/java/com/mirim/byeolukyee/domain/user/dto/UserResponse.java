50bf901828e4182d569d8f76d9a3094822e73bfb
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-26 10:48:52 +0900

    feat: 프로필 사진 url 필드 추가

--- src/main/java/com/mirim/byeolukyee/domain/user/dto/UserResponse.java
+++ src/main/java/com/mirim/byeolukyee/domain/user/dto/UserResponse.java
@@ -13,14 +13,16 @@ public class UserResponse extends Response {
     private final String name;
     private final String email;
     private final String studentId;
+    private final String profileUrl;
 
     @Builder
-    public UserResponse(Boolean isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt, Long id, String name, String email, String studentId) {
+    public UserResponse(Boolean isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt, Long id, String name, String email, String studentId, String profileUrl) {
         super(isDeleted, createdAt, updatedAt);
         this.id = id;
         this.name = name;
         this.email = email;
         this.studentId = studentId;
+        this.profileUrl = profileUrl;
     }
 
     public static UserResponse from(User user) {
@@ -32,6 +34,7 @@ public class UserResponse extends Response {
                 .name(user.getName())
                 .email(user.getEmail())
                 .studentId(user.getStudentId())
+                .profileUrl(user.getProfileUrl())
                 .build();
     }
 }

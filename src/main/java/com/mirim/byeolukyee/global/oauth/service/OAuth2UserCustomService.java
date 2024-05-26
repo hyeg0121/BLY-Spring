3118800222f85d4190980c92200e6256fb31bb8b
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-26 10:48:32 +0900

    feat: 유저 저장 시 hd 검증, 프로필 사진 url, 학번 저장

--- src/main/java/com/mirim/byeolukyee/global/oauth/service/OAuth2UserCustomService.java
+++ src/main/java/com/mirim/byeolukyee/global/oauth/service/OAuth2UserCustomService.java
@@ -3,6 +3,7 @@ package com.mirim.byeolukyee.global.oauth.service;
 
 import com.mirim.byeolukyee.domain.user.entity.User;
 import com.mirim.byeolukyee.domain.user.repository.UserRepository;
+import com.mirim.byeolukyee.global.exception.user.DuplicateEmailException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -28,15 +29,28 @@ public class OAuth2UserCustomService extends DefaultOAuth2UserService {
 
     private User saveOrUpdate(OAuth2User oAuth2User) {
         Map<String, Object> attributes = oAuth2User.getAttributes();
+        String hd = (String) attributes.get("hd");
+
+        if (!hd.equals("e-mirim.hs.kr")) {
+            throw DuplicateEmailException.EXCEPTION;
+        }
+
         String email = (String) attributes.get("email");
-        String name = (String) attributes.get("name");
+        String name = (String) attributes.get("given_name");
+        String studentId = ((String) attributes.get("family_name")).split("_")[0];
+
+        String profileUrl = (String) attributes.get("picture");
+
+        System.out.println(attributes.toString());
+
         User user = userRepository.findByEmail(email)
                 .map(entity -> entity.update(name))
                 .orElse(User.builder()
                         .email(email)
                         .name(name)
                         .password("password12!")
-                        .studentId("1234")
+                        .profileUrl(profileUrl)
+                        .studentId(studentId)
                         .build());
 
         return userRepository.save(user);

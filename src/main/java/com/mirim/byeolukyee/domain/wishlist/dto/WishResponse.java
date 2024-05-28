7df790df75b45cffe630102d3267cb857f22c3ed
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-28 17:29:04 +0900

    feat: WishResponse 추가

---
+++ src/main/java/com/mirim/byeolukyee/domain/wishlist/dto/WishResponse.java
@@ -0,0 +1,22 @@
+package com.mirim.byeolukyee.domain.wishlist.dto;
+
+import com.mirim.byeolukyee.domain.wishlist.entity.Wish;
+import lombok.Getter;
+
+@Getter
+public class WishResponse {
+    private final Long id;
+    private final Long userId;
+    private final Long postId;
+
+
+    public WishResponse(Long id, Long userId, Long postId) {
+        this.id = id;
+        this.userId = userId;
+        this.postId = postId;
+    }
+
+    public static WishResponse from(Wish wish) {
+        return new WishResponse(wish.getId(), wish.getUser().getId(), wish.getPost().getId());
+    }
+}

5a89fd71fc2c9c56f38053fa7496c9893b12ee23
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-29 13:53:11 +0900

    fix: wish response에 isLiked 추가

--- src/main/java/com/mirim/byeolukyee/domain/wish/dto/WishResponse.java
+++ src/main/java/com/mirim/byeolukyee/domain/wish/dto/WishResponse.java
@@ -8,15 +8,17 @@ public class WishResponse {
     private final Long id;
     private final Long userId;
     private final Long postId;
+    private final boolean isLiked;
 
 
-    public WishResponse(Long id, Long userId, Long postId) {
+    public WishResponse(Long id, Long userId, Long postId, boolean isLiked) {
         this.id = id;
         this.userId = userId;
         this.postId = postId;
+        this.isLiked = isLiked;
     }
 
     public static WishResponse from(Wish wish) {
-        return new WishResponse(wish.getId(), wish.getUser().getId(), wish.getPost().getId());
+        return new WishResponse(wish.getId(), wish.getUser().getId(), wish.getPost().getId(), wish.isLiked());
     }
 }

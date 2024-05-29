3f984ef1bee168037ec49de06c8867a631692c67
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-29 13:54:20 +0900

    feat: findByUserIdAndPostId 추가

--- src/main/java/com/mirim/byeolukyee/domain/wish/repository/WishRepository.java
+++ src/main/java/com/mirim/byeolukyee/domain/wish/repository/WishRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
     List<Wish> findByUserId(Long userId);
     List<Wish> findByPostId(Long postId);
+    Optional<Wish> findByUserIdAndPostId(Long userId, Long postId);
 }

f65b5f61f9ccaa049322ba75287407a047051368
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-10 22:10:32 +0900

    feat: WebSocketConfig 추가

--- src/main/java/com/mirim/byeolukyee/global/config/WebSocketConfig.java
+++ src/main/java/com/mirim/byeolukyee/global/config/WebSocketConfig.java
@@ -7,16 +7,16 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker   // 웹 소켓 메세지를 다룰 수 있게 허용
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS(); // 커넥션을 맺는 경로 설정. 만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/chatroom", "/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); // 발행자가 "/topic"의 경로로 메시지를 주면 구독자들에게 전달
-        config.setApplicationDestinationPrefixes("/app"); // 발행자가 "/app"의 경로로 메시지를 주면 가공을 해서 구독자들에게 전달
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/your-websocket-endpoint").withSockJS();
     }
 }

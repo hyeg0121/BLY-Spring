0b8e107d32eb6d9a1e20aade88c6f3a56f589aa8
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-10 19:59:39 +0900

    feat: ChatRoomController 추가

---
+++ src/main/java/com/mirim/byeolukyee/domain/chatroom/controller/ChatRoomController.java
@@ -0,0 +1,37 @@
+package com.mirim.byeolukyee.domain.chatroom.controller;
+
+import com.mirim.byeolukyee.domain.chatroom.dto.AddChatRoomRequest;
+import com.mirim.byeolukyee.domain.chatroom.dto.ChatRoomResponse;
+import com.mirim.byeolukyee.domain.chatroom.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chatrooms")
+@RequiredArgsConstructor
+public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    @GetMapping
+    public ResponseEntity<List<ChatRoomResponse>> getAllChatRooms() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(chatRoomService.findAllChatRooms());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ChatRoomResponse> getChatRoomById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(chatRoomService.findChatRoomById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ChatRoomResponse> createChatRoom(@RequestBody AddChatRoomRequest addChatRoomRequest) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(chatRoomService.createChatRoom(addChatRoomRequest));
+    }
+}

8e9d4780206d6ec021fc42a40ac4d4d62cccb32c
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-18 22:41:47 +0900

    refactor

--- src/main/java/com/mirim/byeolukyee/ByeoLukYeeApplication.java
+++ src/main/java/com/mirim/byeolukyee/ByeoLukYeeApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class ByeoLukYeeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ByeoLukYeeApplication.class, args);
     }
-
 }
